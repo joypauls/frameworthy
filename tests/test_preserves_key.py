6d02ac31049982c99d078853cb5937ecec1395dd
joypauls <joypaulsen3@gmail.com>
2026-08-27 21:16:15 -0500

    parameterize test for key types

--- tests/test_preserves_key.py
+++ tests/test_preserves_key.py
@@ -3,11 +3,21 @@ import pytest
 import frameworthy as fw
 
 
+@pytest.mark.parametrize(
+    "before_keys,after_keys",
+    [
+        pytest.param([1, 2, 2, 3], [1, 2, 3, 3], id="int"),
+        pytest.param(["a", "b", "b", "c"], ["a", "b", "c", "c"], id="str"),
+        pytest.param([1.0, 2.0, 2.0, 3.0], [1.0, 2.0, 3.0, 3.0], id="float"),
+    ],
+)
 def test_detects_changed_multiplicity(
     frame_factory,
+    before_keys,
+    after_keys,
 ):
-    before = frame_factory({"id": [1, 2, 2, 3]})
-    after = frame_factory({"id": [1, 2, 3, 3]})
+    before = frame_factory({"id": before_keys})
+    after = frame_factory({"id": after_keys})
 
     # fw.expect(after, relative_to=before).preserves_rows()
 
