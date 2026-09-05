762c120ecc84e047df9eb48317cacbbdc00a7f69
joypauls <joypaulsen3@gmail.com>
2026-09-04 19:56:39 -0500

    add constant for inference method

--- src/frameworthy/_constants.py
+++ src/frameworthy/_constants.py
@@ -1,4 +1,10 @@
+from typing import Literal
+
 NULL_KEY = object()
 
 DEFAULT_ALPHA = 0.05
 DEFAULT_N_RESAMPLES = 10_000
+
+# inference strategy options
+InferenceMethod = Literal["analytical", "bootstrap"]
+DEFAULT_INFERENCE_METHOD: InferenceMethod = "analytical"
