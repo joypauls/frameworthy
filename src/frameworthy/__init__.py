82f5d3622f2b48b6aff9395b3acba7b9ef7d3095
joypauls <joypaulsen3@gmail.com>
2026-09-06 17:15:32 -0500

    export new functions

--- src/frameworthy/__init__.py
+++ src/frameworthy/__init__.py
@@ -1,9 +1,10 @@
 from frameworthy._errors import FrameworthyAssertionError
 from frameworthy.check import Check, MeanCheck, RateCheck, check
 from frameworthy.decision import Decision
-from frameworthy.results import EquivalenceResult
+from frameworthy.results import ChangeResult, EquivalenceResult
 
 __all__ = [
+    "ChangeResult",
     "Check",
     "Decision",
     "EquivalenceResult",
