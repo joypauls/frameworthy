f8b1ab09355b6a69bc7758bdb78e1b081a75cc29
joypauls <joypaulsen3@gmail.com>
2026-09-06 21:26:39 -0500

    less confusing terminology

--- scripts/generate_e2e_datasets.py
+++ scripts/generate_e2e_datasets.py
@@ -20,30 +20,30 @@ def generate_bernoulli(size: int, p: float) -> np.ndarray:
 
 def main():
     normal_before = generate_normal(0, 1, ROWS)
-    binomial_before = generate_bernoulli(ROWS, p=0.5)
+    bernoulli_before = generate_bernoulli(ROWS, p=0.5)
 
     normal_after_unchanged = generate_normal(0, 1, ROWS)
-    binomial_after_unchanged = generate_bernoulli(ROWS, p=0.5)
+    bernoulli_after_unchanged = generate_bernoulli(ROWS, p=0.5)
 
     normal_after_changed = generate_normal(0.5, 1, ROWS)
-    binomial_after_changed = generate_bernoulli(ROWS, p=0.7)
+    bernoulli_after_changed = generate_bernoulli(ROWS, p=0.7)
 
     df_before = pd.DataFrame(
         {
             "normal": normal_before,
-            "binomial": binomial_before,
+            "bernoulli": bernoulli_before,
         }
     )
     df_after_changed = pd.DataFrame(
         {
             "normal": normal_after_changed,
-            "binomial": binomial_after_changed,
+            "bernoulli": bernoulli_after_changed,
         }
     )
     df_after_unchanged = pd.DataFrame(
         {
             "normal": normal_after_unchanged,
-            "binomial": binomial_after_unchanged,
+            "bernoulli": bernoulli_after_unchanged,
         }
     )
 
