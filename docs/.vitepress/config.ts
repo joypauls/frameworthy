588bd2e2806ddd01c94f0db53fe87f70723e40ac
joypauls <joypaulsen3@gmail.com>
2026-09-08 20:36:41 -0500

    start some methodology notes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 
   themeConfig: {
     nav: [
-      { text: "Guide", link: "/getting-started" },
+      { text: "Docs", link: "/getting-started" },
     //   { text: "API", link: "/api/" }
     ],
 
@@ -22,8 +22,14 @@ export default defineConfig({
       {
         text: "Guide",
         items: [
-          { text: "Home", link: "/" },
+          // { text: "Home", link: "/" },
           { text: "Getting Started", link: "/getting-started" }
+        ]
+      },
+      {
+        text: "Internals",
+        items: [
+          { text: "Methodology", link: "/methodology" }
         ]
       },
       // {
